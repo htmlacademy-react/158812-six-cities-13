a29833a4bf83d253a98ccde3f52e8f72445f793c
Alena Karenina <karena1q84@gmail.com>
2023-08-25 19:02:03 +0500

    fix: changes const RATING_VALUES

--- src/const.ts
+++ src/const.ts
@@ -56,8 +56,8 @@ export const RATING_VALUES = [
   {value: 5, title: 'perfect'},
   {value: 4, title: 'good'},
   {value: 3, title: 'not bad'},
-  {value: 2, title: 'terribly'},
-  {value: 1, title: 'badly'},
+  {value: 2, title: 'badly'},
+  {value: 1, title: 'terribly'},
 ];
 export const MIN_COMMENT_LENGTH = 50;
 export const MAX_COMMENT_LENGTH = 300;
